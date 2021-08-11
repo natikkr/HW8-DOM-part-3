2662434ed48214cc3da95cd620d9f4275477627e
natikkr <82267830+natikkr@users.noreply.github.com>
2021-08-11 19:02:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,4 +18,28 @@
 recursion(1,5)
 
 
+window.setInterval(function clockTimer(){
 
+    let date = new Date();
+    let time = [date.getHours(),date.getMinutes(),date.getSeconds()]; 
+    let current_time = [time[0],time[1],time[2]].join(':');
+    let clock = document.getElementById("clock");
+    let timeS = [date.getHours(),date.getMinutes()]; 
+    let current_timeS = [time[0],time[1]].join(':');
+
+    if(time[0] < 10){time[0] = "0"+ time[0];}
+    if(time[1] < 10){time[1] = "0"+ time[1];}
+    if(time[2] < 10){time[2] = "0"+ time[2];}
+
+    if (clock.classList.contains('active')){
+        clock.innerHTML = current_timeS;
+    }
+    else{
+        clock.innerHTML = current_time;
+    }
+    
+    document.querySelector('div').addEventListener('click', (event) =>{
+        event.target.classList.toggle('active');
+       
+    });
+})
